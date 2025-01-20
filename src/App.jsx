fb3a7805c92f8ea32f7c33e3ddb894ae737654b1
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-01-20 12:22:33 +0530

    Custom Google Fonts and Tailwind configuration code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <>
-      <h1 className="text-4xl font-semibold">Book Finder Application with React - iNeuron</h1>
+      <h1 className="text-4xl font-semibold font-poppins">
+        Book Finder Application with React - iNeuron
+      </h1>
     </>
   );
 };
