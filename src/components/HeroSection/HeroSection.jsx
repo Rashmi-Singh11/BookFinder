d560a92da62de5210a161320a33a565183b74d68
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-01-23 12:14:15 +0530

    HeroSection Purple-Gradient with Cover Image added

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,18 +1,19 @@
 // HeroSection component serves as the introductory section of the application
 // It includes a title, description, and a search form for users to find books.
 import SearchForm from "../SearchForm/SearchForm.jsx";
+import libraryImg from "../../images/library-img.jpg"; // Importing the library image
 
 const HeroSection = () => {
   return (
-    <section className="overflow-auto h-64 flex items-center justify-center min-h-[75vh] bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-      <div className="flex flex-col items-center text-center max-w-[770px] px-4">
-       {/* Main title of the Hero section */}
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 uppercase">
+    <section className="relative overflow-auto h-64 flex items-center justify-center min-h-[75vh] bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+      <div className="absolute z-20 flex flex-col items-center text-center max-w-[770px] px-4">
+        {/* Main title of the Hero section */}
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 uppercase">
           Find Your Book of Choice
         </h1>
 
         {/* Description paragraph providing context for the application */}
-        <p className="text-base sm:text-lg md:text-xl mb-6">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-6">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
           sapiente quibusdam consequatur perspiciatis facere laboriosam non
           nesciunt at id repudiandae, modi iste? Eligendi, rerum!
@@ -21,6 +22,13 @@ const HeroSection = () => {
         {/* Align SearchForm in flex column */}
         <SearchForm />
       </div>
+
+      {/* Library image with low opacity */}
+      <img
+        src={libraryImg} // Source of the library image
+        alt="Library Background" // Alt text for accessibility
+        className="absolute inset-0 w-full h-full object-cover opacity-60" // Full coverage with low opacity
+      />
     </section>
   );
 };
