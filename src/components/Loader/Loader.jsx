1d3185a05572d936bf04e9434f5ea48aedb3a61c
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-01-23 12:08:02 +0530

    Loader component code added

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-16 h-16 border-8 border-t-8 border-blue-500 border-t-purple-600 rounded-full animate-spin"></div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
